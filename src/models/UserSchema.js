8aded08f9fa32f3f16fd83efb4ec49936d48456b
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-03-14 19:36:24 -0300

    adding pre-save middleware to UserSchema

--- src/models/UserSchema.js
+++ src/models/UserSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const sha256 = require('sha256');
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -18,6 +19,12 @@ const UserSchema = mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+UserSchema.pre('save', async function(next) {
+    const hash = sha256(`${sha256(this.password)}${sha256(process.env.SALT)}`);
+    this.password = hash;
+    next();
 });
 
 const User = mongoose.model('user', UserSchema);
